dc8657cbbc118e7e0fe4cc143077dc949c1c94a4
abao <abao@juanpi.com>
2017-09-07 16:41:32 +0800

    增加c++处理json实现

---
+++ README.md
@@ -0,0 +1,4 @@
+# daily
+每天一段c++
+
+> * [使用rapidjson处理json格式](/src/json.cpp)
---
+++ src/json.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
+
+int main()
+{
+    // 1. 把 JSON 解析至 DOM。
+    const char* json = "{\"p\":\"r\",\"s\":10}";
+    Document d;
+    d.Parse(json);
+    // 2. 利用 DOM 作出修改。
+    Value& s = d["s"];
+    s.SetInt(s.GetInt() + 1);
+    // 3. 把 DOM 转换（stringify）成 JSON。
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << buffer.GetString() << std::endl;
+    system("pause");
+    return 0;
+}
